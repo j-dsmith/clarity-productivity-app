2cfbae39b4f9800bf8b911dc4ffba813f2028150
Jamey Smith <smith.jameyd@gmail.com>
2022-02-04 09:01:16 -0600

    Still getting mixed content warnings when deployed in firefox, safari throwing promise rejection error. Need further testing before implementing feature live

--- components/dashboard/openweather/index.js
+++ components/dashboard/openweather/index.js
@@ -112,11 +112,12 @@ function Forecast() {
             handler={getForecastByName}
           />
           {/* Issue with user coordinates when pushed to vercel */}
-          <UIBtn
+
+          {/* <UIBtn
             icon={<MdOutlineLocationOn />}
             color="var(--text-dark)"
             handler={getUserCoordinates}
-          />
+          /> */}
         </InputGroup>
         <p className="search-format">
           For US Locations, format as (City, State, Country)
