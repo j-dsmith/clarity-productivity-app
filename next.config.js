48ec9f03e6dedb3ae4ee6a44328f3b8bd2498da2
Jamey Smith <smith.jameyd@gmail.com>
2022-02-08 09:27:25 -0600

    config issue

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    MONGODB_URI:
+      "mongodb+srv://j-dsmith:J4m3y_tul54@cluster0.zrtta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    OPENWEATHER_API_KEY: "44b56dcf9b479c6487cb1462eccb3ad7",
+    NEXTAUTH_SECRET: "Yr4cHm6oJhFNu8gtOVxOI4pk69ZShY1Rl6AqoRVBdwQ=",
+    GUEST_EMAIL: "guest@clarity-productivity.com",
+    GUEST_PASSWORD: "guest",
+  },
+  images: {
+    domains: ["openweathermap.org"],
+  },
+};
